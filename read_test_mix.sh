ad000354ebba43600a23ee0628d7bb638c58914d
fasape <mstepten@163.com>
2018-04-13 11:22:48 +0800

    master-tbase core

--- read_test_mix.sh
+++ read_test_mix.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 OSDATE=$(echo `date +%Y%m%d_%H_%M_%S`)
-sh write.sh >log/write_log.out 2>&1 &
+sh write.sh >log/write_log_${OSDATE}.out 2>&1 &
 sh read_test.sh >log/read_test_mix_log_${OSDATE}.out 2>&1 &
 echo "program run success ,if wanting to see runing log please see log/"
